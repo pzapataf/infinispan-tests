79439f9785138b3c97c15a0a2528190ad5557ed0
Pedro Zapata <pzapataf@gmail.com>
2016-06-02 09:24:48 +0200

    Some POC

---
+++ src/test/java/demo/Coords.java
@@ -0,0 +1,63 @@
+package demo;
+
+import java.io.Serializable;
+
+/**
+ * Created by pzapataf on 5/31/16.
+ */
+public class Coords implements Serializable {
+    private int x;
+    private int y;
+
+    public Coords(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Coords() {
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Coords coords = (Coords) o;
+
+        if (x != coords.x) return false;
+        if (y != coords.y) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Coords{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
